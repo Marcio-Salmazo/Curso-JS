502813fefd5dd25a0a762d096eb41776d3de6a38
Marcio Ramos <marcio.salmazo19@gmail.com>
2023-01-04 16:13:09 -0300

    first commit

---
+++ ConsoleLogClass/ConsoleLog.js
@@ -0,0 +1,7 @@
+ console.log('Hello World');
+ //obs: ponto e vírgula é opicional
+ //obs2: Aspas simples, duplas e crase exibem string
+
+ console.log("Hello");
+ console.log('Hello');
+ console.log(`Hello`);
---
+++ ConsoleLogClass/Exercicio.js
@@ -0,0 +1,8 @@
+/*
+Exibir a String:
+Meu nome é "NOME". Estou aprendendo JS às 10 da manhã.
+*/
+
+var nome = '"Marcio"';
+var hora = 15
+console.log('Meu nome é', nome, '. Estou aprendendo JS às', hora, 'da manhã.');
